f4ffaa00586e8c8b1e221ae6904d72641f60299c
Joonas Sandell <me@joonassandell.com>
2024-04-27 17:53:23 +0300

    Add missing autoloader, add name for the register plugin

--- web/wp-content/mu-plugins/register-theme-directory.php
+++ web/wp-content/mu-plugins/register-theme-directory.php
@@ -1,2 +1,6 @@
 <?php
+/**
+ * Plugin Name: Register Theme Directory
+ * Description: Register the custom wp-content/themes/ directory
+ */
 register_theme_directory(ABSPATH . 'wp-content/themes/');
